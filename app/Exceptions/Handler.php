0835880b141f0ac2d8dec567aeab65d9caea9ffb
Shaon Majumder <smazoomder@gmail.com>
2022-10-12 17:10:28 +0600

    cors solved

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,19 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Validation\ValidationException;
+use PDOException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
+use TypeError;
 
 class Handler extends ExceptionHandler
 {
@@ -27,15 +40,53 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
+    function render($request, Throwable $e): JsonResponse
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        Log::info($e->getMessage());
+        $statusCode = ResponseCode::HTTP_INTERNAL_SERVER_ERROR;
+        $message = "unknown error";
+        $errors = [];
+        if ($e instanceof HttpResponseException) {
+            $statusCode = $e->getCode() !== 0 ? $e->getCode() : ResponseCode::HTTP_INTERNAL_SERVER_ERROR;
+            $message = $e->getMessage();
+
+        } elseif ($e instanceof ModelNotFoundException) {
+            $statusCode = $e->getCode() !== 0 ? $e->getCode() : ResponseCode::HTTP_NOT_FOUND;
+            $message = $statusCode === ResponseCode::HTTP_NOT_FOUND ? $e->getMessage() : $e->getMessage();
+
+        } elseif ($e instanceof NotFoundHttpException ) {
+            $statusCode = $e->getCode() !== 0 ? $e->getCode() : ResponseCode::HTTP_NOT_FOUND;
+            $message = $statusCode === ResponseCode::HTTP_NOT_FOUND ? $e->getMessage() : $e->getMessage();
+        }
+        elseif ($e instanceof PDOException) {
+            $message = 'PDO Error';
+
+        } elseif ($e instanceof TypeError) {
+            $message = 'Type Error';
+            /*$message = $e->getMessage();*/
+
+        } elseif ($e instanceof AuthenticationException) {
+            $statusCode = $e->getCode() !== 0 ? $e->getCode() : ResponseCode::HTTP_INTERNAL_SERVER_ERROR;
+            $message = $e->getMessage();
+
+        } elseif ($e instanceof AuthorizationException) {
+            $statusCode = $e->getCode() !== 0 ? $e->getCode() : ResponseCode::HTTP_INTERNAL_SERVER_ERROR;
+            $message = $e->getMessage();
+
+        } elseif ($e instanceof ValidationException) {
+            $statusCode = ResponseCode::HTTP_UNPROCESSABLE_ENTITY;
+            $message = $e->getMessage();
+            $errors = $e->errors();
+
+        } else if ($e instanceof Exception) {
+            $statusCode = $e->getCode() !== 0 ? $e->getCode() : ResponseCode::HTTP_INTERNAL_SERVER_ERROR;
+            $message = $e->getMessage();
+        }
+
+        return Response::json([
+            "status_code"=>$statusCode,
+            "message" => $message,
+            "errors" => $errors
+        ] ,$statusCode);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\Auth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::resource('products',ProductController::class);
-
+Route::post('/login', [LoginController::class, "login"]);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
